526eb3425de0cec6bd095cbe8dd0ada77e6c51d6
Lucas Mello <77343991+LucasMelll0@users.noreply.github.com>
2025-10-16 20:24:24 -0300

    feat: Implements get user endpoint.

--- src/main/java/com/example/auth_api/controllers/UserController.java
+++ src/main/java/com/example/auth_api/controllers/UserController.java
@@ -1,17 +1,27 @@
 package com.example.auth_api.controllers;
 
+import com.example.auth_api.domain.user.User;
+import com.example.auth_api.dto.GetLoggedUserResponseDTO;
+import com.example.auth_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
+    private final UserRepository repository;
+
+
     @GetMapping
-    public ResponseEntity<String> getUser() {
-        return ResponseEntity.ok("Success");
+    public ResponseEntity<GetLoggedUserResponseDTO> getUser(@AuthenticationPrincipal User loggedUser) {
+        final GetLoggedUserResponseDTO response = new GetLoggedUserResponseDTO(loggedUser.getName(), loggedUser.getEmail());
+        return ResponseEntity.ok(response);
     }
 
 }
---
+++ src/main/java/com/example/auth_api/dto/GetLoggedUserResponseDTO.java
@@ -0,0 +1,4 @@
+package com.example.auth_api.dto;
+
+public record GetLoggedUserResponseDTO(String name, String email) {
+}
