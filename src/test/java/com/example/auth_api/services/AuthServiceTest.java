c2916afca3e4691939c2185732f2cf48af8a99d6
Lucas Mello <77343991+LucasMelll0@users.noreply.github.com>
2025-10-24 15:15:26 -0300

    tests: Add login test case 1.

--- src/test/java/com/example/auth_api/services/AuthServiceTest.java
+++ src/test/java/com/example/auth_api/services/AuthServiceTest.java
@@ -1,5 +1,8 @@
 package com.example.auth_api.services;
 
+import com.example.auth_api.domain.user.User;
+import com.example.auth_api.dtos.LoginRequestDTO;
+import com.example.auth_api.dtos.LoginResponseDTO;
 import com.example.auth_api.dtos.RegisterRequestDTO;
 import com.example.auth_api.dtos.RegisterResponseDTO;
 import com.example.auth_api.infra.security.TokenService;
@@ -11,6 +14,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
@@ -36,6 +41,28 @@ class AuthServiceTest {
     @Test
     @DisplayName("Should login successfully when everything is OK")
     void loginCase1() {
+        //Arrange
+        String username = "John";
+        String email = "email@mail.com";
+        String rawPassword = "John123";
+        String encodedPassword = "encoded123";
+        String fakeToken = "token.jwt.token";
+        LoginRequestDTO request = new LoginRequestDTO(email, rawPassword);
+        User user = new User("id", username, email, encodedPassword);
+        when(userRepository.findByEmail(email)).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(rawPassword, encodedPassword)).thenReturn(true);
+        when(tokenService.generateToken(email)).thenReturn(fakeToken);
+
+
+        //Act
+        LoginResponseDTO result = this.authService.login(request);
+
+        //Assert
+        assertThat(result.token()).isEqualTo(fakeToken);
+        assertThat(result.name()).isEqualTo(username);
+        verify(userRepository).findByEmail(email);
+        verify(passwordEncoder).matches(rawPassword, encodedPassword);
+        verify(tokenService).generateToken(email);
 
     }
 
@@ -60,12 +87,12 @@ class AuthServiceTest {
         String rawPassword = "John123";
         String encodedPassword = "encoded123";
         String fakeToken = "token.jwt.token";
-        RegisterRequestDTO requestDTO = new RegisterRequestDTO(username, email, rawPassword);
+        RegisterRequestDTO request = new RegisterRequestDTO(username, email, rawPassword);
         when(passwordEncoder.encode(rawPassword)).thenReturn(encodedPassword);
         when(tokenService.generateToken(email)).thenReturn(fakeToken);
 
         // Act
-        RegisterResponseDTO result = this.authService.register(requestDTO);
+        RegisterResponseDTO result = this.authService.register(request);
 
         //Assert
         assertThat(result.token()).isEqualTo(fakeToken);
@@ -74,10 +101,7 @@ class AuthServiceTest {
     }
 
     @Test
+    @DisplayName("Should throw EmailAlreadyRegisteredException when the email is already registered")
     void registerCase2() {
-    }
-
-    @Test
-    void registerCase3() {
     }
 }
